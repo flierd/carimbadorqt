374910f334e8c9902b155c60f21834630c4eba1c
flierd <flierd@users.noreply.github.com>
2019-10-24 14:22:53 -0300

    mais funcionalidades

--- CarimbadorQt.py
+++ CarimbadorQt.py
@@ -1,4 +1,5 @@
 import sys
+from os import listdir
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import Qt
@@ -16,6 +17,12 @@ class CarimbadorQt(QWidget):
 
     def initui(self):
         self.img = Image.open('./res/vazio.png')
+        self.im = self.img.copy()
+        self.im.thumbnail([500,500],Image.ANTIALIAS)
+
+        self.logo = Image.open('./res/vazio.png')
+        self.imlogo = self.logo.copy()
+        self.imlogo.thumbnail([100,100],Image.ANTIALIAS)        
 
         btnAbrir = QPushButton('Abrir')
         btnAbrir.clicked.connect(self.eventoOpen)
@@ -23,10 +30,23 @@ class CarimbadorQt(QWidget):
         btnCapturar = QPushButton('Capturar')
         btnCapturar.clicked.connect(self.eventoCaptura)
 
+        self.spinEscala = QDial()
+        self.spinEscala.setValue(30)
+        self.spinEscala.setNotchesVisible(True)
+        self.spinEscala.setToolTip('Escala do Logo')
+
         self.labelImagem = QLabel()
         self.imagemqt = ImageQt.ImageQt(self.img)
         self.labelImagem.setPixmap(QPixmap.fromImage(self.imagemqt))
         self.labelImagem.setObjectName("image")
+
+        self.listaLogo = QListWidget()
+        self.listaLogo.addItems(self.getLogoList())
+        self.listaLogo.clicked.connect(self.eventoMudaLogo)
+
+        self.labelLogo = QLabel()
+        self.logoqt = ImageQt.ImageQt(self.imlogo)
+        self.labelLogo.setPixmap(QPixmap.fromImage(self.logoqt))
 
         self.setGeometry(100, 30, 500, 500)
         self.setWindowTitle("Carimbador Qt")
@@ -34,8 +54,13 @@ class CarimbadorQt(QWidget):
 
         layout = QGridLayout()
         layout.addWidget(btnAbrir, 0, 0,Qt.AlignTop)
-        layout.addWidget(btnCapturar, 0, 0,Qt.AlignTrailing)
+        layout.addWidget(btnCapturar, 0, 0,Qt.AlignVCenter)
         layout.addWidget(self.labelImagem,0,1,1,1)
+        layout.addWidget(self.spinEscala,0,0,Qt.AlignBottom)
+
+
+        layout.addWidget(self.listaLogo,1,0,Qt.AlignVCenter)
+        layout.addWidget(self.labelLogo,1,1,1,1)
 
 
         self.setLayout(layout)
@@ -45,7 +70,7 @@ class CarimbadorQt(QWidget):
         f = QFileDialog.getOpenFileName(None,'Abrir',str(Path.home())+'/Pictures','PNG (*.png);;JPEG (*.jpg)')
         self.img = Image.open(f[0],'r')
         im = self.img.copy()
-        im.thumbnail([600,600],Image.ANTIALIAS)
+        im.thumbnail([500,500],Image.ANTIALIAS)
         self.imagemqt = ImageQt.ImageQt(im)
         self.labelImagem.setPixmap(QPixmap.fromImage(self.imagemqt))
     
@@ -70,7 +95,24 @@ class CarimbadorQt(QWidget):
                 
         box = (x1,y1,x2,y2)
         self.img = ImageGrab.grab(box)
-        self.img.show()
+        im = self.img.copy()
+        im.thumbnail([500,500],Image.ANTIALIAS)
+        self.imagemqt = ImageQt.ImageQt(im)
+        self.labelImagem.setPixmap(QPixmap.fromImage(self.imagemqt))
+    
+    def getLogoList(self):
+        arqsLogos=[]
+        for f in listdir('./res'):
+            if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.PNG') or f.endswith('.JPG'):
+                arqsLogos.append(f)
+        return arqsLogos
+
+    def eventoMudaLogo(self):
+        self.logo = Image.open('./res/'+self.listaLogo.currentItem().text())
+        self.imlogo = self.logo.copy()
+        self.imlogo.thumbnail([100,100],Image.ANTIALIAS)
+        self.logoqt = ImageQt.ImageQt(self.imlogo)
+        self.labelLogo.setPixmap(QPixmap.fromImage(self.logoqt))
 
 
 if __name__ == '__main__':
